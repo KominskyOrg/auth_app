98b509461e3f88949f5e6b3d85e59df7071a38aa
jaredkominsky <kominskyjared@gmail.com>
2024-10-06 23:21:13 -0500

    [feat] CD Automation
    - updating eks module to v1.1 to use dyanmic env blocks

--- tf/main.tf
+++ tf/main.tf
@@ -16,13 +16,23 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
+locals {
+  node_env = var.env == "staging" ? "development" : var.env == "prod" ? "production" : var.env
+}
+
 module "eks" {
-  source           = "git::https://github.com/KominskyOrg/kom_tf_modules.git?ref=v1.0//eks"
+  source           = "git::https://github.com/KominskyOrg/kom_tf_modules.git//eks?ref=v1.1"
   eks_service_name = "${local.stack_name}-${local.microservice_type}"
   env              = local.env
   ecr_url          = aws_ecr_repository.app_ecr.repository_url
   image_tag        = var.image_tag
-  node_selector    = {
+  node_selector = {
     role = local.node_selector
+  }
+  service_port        = 8080
+  service_target_port = 3000
+  env_vars = {
+    AUTH_API_URL = "https://${var.env}.jaredkominsky.com/api/auth"
+    NODE_ENV     = local.node_env
   }
 }
